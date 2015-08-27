9aa0075551d0695da1c9e6526055b0911b254df7
kirillmagadan <kirill@reefstudio.ru>
2015-08-27 09:57:04 +0300

    Revert "Revert "Version 0.1""
    This reverts commit 9e815d1a44307e75adb717388e8db9d4411cdfa2.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    // slider
+    $('.header--slider').smSlider({
+        transition : 'fader',
+        duration : 500,
+        autoPlay : false,
+        autoArr : false,
+        pagination : true
+    });
+
+
+});
